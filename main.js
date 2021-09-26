7cb7f6851b40690c14a18f1b7cfbdf8b5a00044d
Theo Lebegue <theo.lebegue1@gmail.com>
2021-09-26 22:31:18 +0200

    ft/ Game is working

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-let totalScorePlayer1 = document.getElementById("totalScorePlayer1");
-let totalScorePlayer2 = document.getElementById("totalScorePlayer2");
+let displayScorePlayer1 = document.getElementById("totalScorePlayer1");
+let displayScorePlayer2 = document.getElementById("totalScorePlayer2");
+let totalScore1 = 0;
+let totalScore2 = 0;
 let currentScorePlayer1 = document.getElementById("currentScorePlayer1");
 let currentScorePlayer2 = document.getElementById("currentScorePlayer2");
 let rollButton = document.getElementById("rollButton");
@@ -7,21 +9,67 @@ let holdButton = document.getElementById("holdButton");
 let diceImage = document.getElementById("diceImage");
 let player1 = document.getElementById("player1");
 let player2 = document.getElementById("player2");
+let current = 0;
+let newGameButton = document
+  .getElementById("newGame")
+  .addEventListener("click", () => {
+    location.reload();
+  });
+let textMessage = document.getElementById("textMessage");
+
+let goal = 100;
+let lose = 1;
 
 function randomDiceNumber() {
-  let randomNum = Math.floor(Math.random() * 6 + 1);
+  let randomnum = Math.floor(Math.random() * 6 + 1);
+  diceImage.setAttribute("src", `./assets/img/de${randomnum}.png`);
+  return randomnum;
 }
 
 function changeColorPlayer() {
   if (player1.classList.contains("activePlayer")) {
     player1.classList.toggle("activePlayer");
     player2.classList.toggle("activePlayer");
-  }
-  else if (player2.classList.contains("activePlayer")) {
+  } else if (player2.classList.contains("activePlayer")) {
     player2.classList.toggle("activePlayer");
     player1.classList.toggle("activePlayer");
   }
 }
+function holding() {
+  if (player1.classList.contains("activePlayer")) {
+    totalScore1 = totalScore1 + current;
+    displayScorePlayer1.innerHTML = totalScore1;
+    current = 0;
+    currentScorePlayer1.innerHTML = current;
+    changeColorPlayer();
+  } else if (player2.classList.contains("activePlayer")) {
+    totalScore2 = totalScore2 + current;
+    displayScorePlayer2.innerHTML = totalScore2;
+    current = 0;
+    currentScorePlayer2.innerHTML = current;
+    changeColorPlayer();
+  }
+}
 
+function rollDice() {
+  let rolled = randomDiceNumber();
+  if (rolled == 1) {
+    textMessage.textContent = "You rolled a 1 ! Turn skipped !";
 
-holdButton.addEventListener("click", changeColorPlayer)
+    current = 0;
+    currentScorePlayer1.innerText = current;
+    currentScorePlayer2.innerText = current;
+    changeColorPlayer();
+  } else {
+    current = current + rolled;
+    textMessage.textContent = `You rolled a ${rolled}, hold or continue ?`;
+    if (player1.classList.contains("activePlayer")) {
+      currentScorePlayer1.innerHTML = current;
+    } else if (player2.classList.contains("activePlayer")) {
+      currentScorePlayer2.innerHTML = current;
+    }
+  }
+}
+console.log(currentScorePlayer1);
+rollButton.addEventListener("click", rollDice);
+holdButton.addEventListener("click", holding);
