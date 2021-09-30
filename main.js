e8b60ced87157217985989ae05fa50b5f832381f
Theo Lebegue <theo.lebegue1@gmail.com>
2021-09-30 11:18:18 +0200

    ft/Win animation and remove event listener if win

--- main.js
+++ main.js
@@ -17,9 +17,24 @@ let newGameButton = document
   });
 let textMessage = document.getElementById("textMessage");
 
-let goal = 100;
+let goal = 1;
 let lose = 1;
-
+function win() {
+  if (totalScore1 >= goal) {
+    textMessage.textContent = "Player 1 is the winner ! New game ?"
+    textMessage.style.animation =
+      "winAnim 1s ease-in 0s infinite reverse forwards";
+    rollButton.removeEventListener("click", rollDice);
+    holdButton.removeEventListener("click", holding);
+  }
+  else if (totalScore2 >= goal) {
+    textMessage.textContent = "Player 2 is the winner ! New game ?"
+    textMessage.style.animation = "winAnim 1s ease-in 0s infinite reverse forwards";
+    
+    rollButton.removeEventListener("click", rollDice);
+    holdButton.removeEventListener("click", holding);
+  }
+}
 function randomDiceNumber() {
   let randomnum = Math.floor(Math.random() * 6 + 1);
   diceImage.setAttribute("src", `./assets/img/de${randomnum}.png`);
@@ -41,6 +56,7 @@ function holding() {
     displayScorePlayer1.innerHTML = totalScore1;
     current = 0;
     currentScorePlayer1.innerHTML = current;
+    win()
     changeColorPlayer();
   } else if (player2.classList.contains("activePlayer")) {
     totalScore2 = totalScore2 + current;
@@ -48,12 +64,13 @@ function holding() {
     current = 0;
     currentScorePlayer2.innerHTML = current;
     changeColorPlayer();
+    win()
   }
 }
 
 function rollDice() {
   let rolled = randomDiceNumber();
-  if (rolled == 1) {
+  if (rolled == lose) {
     textMessage.textContent = "You rolled a 1 ! Turn skipped !";
 
     current = 0;
@@ -70,6 +87,5 @@ function rollDice() {
     }
   }
 }
-console.log(currentScorePlayer1);
 rollButton.addEventListener("click", rollDice);
 holdButton.addEventListener("click", holding);
