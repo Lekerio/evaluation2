573f37b95842f367b54c6d5f366accc9ead03810
Theo Lebegue <theo.lebegue1@gmail.com>
2021-10-03 14:42:06 +0200

    ft/ alt attributes on imgs

--- main.js
+++ main.js
@@ -26,15 +26,15 @@ let lose = 1;
 //Stop the game if the goal is reached and display a little animation
 function win() {
   if (totalScore1 >= goal) {
-    textMessage.textContent = "Player 1 is the winner ! New game ?"
+    textMessage.textContent = "Player 1 is the winner ! New game ?";
     textMessage.style.animation =
       "winAnim 1s ease-in 0s infinite reverse forwards";
     rollButton.removeEventListener("click", rollDice);
     holdButton.removeEventListener("click", holding);
-  }
-  else if (totalScore2 >= goal) {
-    textMessage.textContent = "Player 2 is the winner ! New game ?"
-    textMessage.style.animation = "winAnim 1s ease-in 0s infinite reverse forwards";
+  } else if (totalScore2 >= goal) {
+    textMessage.textContent = "Player 2 is the winner ! New game ?";
+    textMessage.style.animation =
+      "winAnim 1s ease-in 0s infinite reverse forwards";
 
     rollButton.removeEventListener("click", rollDice);
     holdButton.removeEventListener("click", holding);
@@ -47,7 +47,7 @@ function randomDiceNumber() {
   diceImage.setAttribute("src", `./assets/img/de${randomnum}.png`);
   return randomnum;
 }
-//Change player fonction by using classList 
+//Change player fonction by using classList
 function changeColorPlayer() {
   if (player1.classList.contains("activePlayer")) {
     player1.classList.toggle("activePlayer");
@@ -64,7 +64,7 @@ function holding() {
     displayScorePlayer1.innerHTML = totalScore1;
     current = 0;
     currentScorePlayer1.innerHTML = current;
-    win()
+    win();
     changeColorPlayer();
   } else if (player2.classList.contains("activePlayer")) {
     totalScore2 = totalScore2 + current;
@@ -72,7 +72,7 @@ function holding() {
     current = 0;
     currentScorePlayer2.innerHTML = current;
     changeColorPlayer();
-    win()
+    win();
   }
 }
 // Roll the dice function and put the score in the current Score of the active player.
