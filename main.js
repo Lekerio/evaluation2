7fa4963c29a833617320c00fb6660c55802436aa
Theo Lebegue <theo.lebegue1@gmail.com>
2021-10-03 14:40:10 +0200

    ft/ Commenting html and js to make it easier to understand

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Declaration of all necessary variables to play the game
+
 let displayScorePlayer1 = document.getElementById("totalScorePlayer1");
 let displayScorePlayer2 = document.getElementById("totalScorePlayer2");
 let totalScore1 = 0;
@@ -10,15 +12,18 @@ let diceImage = document.getElementById("diceImage");
 let player1 = document.getElementById("player1");
 let player2 = document.getElementById("player2");
 let current = 0;
+// New game button, reload the page.
 let newGameButton = document
   .getElementById("newGame")
   .addEventListener("click", () => {
     location.reload();
   });
 let textMessage = document.getElementById("textMessage");
-
+// Settings goal score and lose number. Easier settings by putting them here
 let goal = 100;
 let lose = 1;
+
+//Stop the game if the goal is reached and display a little animation
 function win() {
   if (totalScore1 >= goal) {
     textMessage.textContent = "Player 1 is the winner ! New game ?"
@@ -35,12 +40,14 @@ function win() {
     holdButton.removeEventListener("click", holding);
   }
 }
+
+// Roll a dice, and change the image according to the score.
 function randomDiceNumber() {
   let randomnum = Math.floor(Math.random() * 6 + 1);
   diceImage.setAttribute("src", `./assets/img/de${randomnum}.png`);
   return randomnum;
 }
-
+//Change player fonction by using classList 
 function changeColorPlayer() {
   if (player1.classList.contains("activePlayer")) {
     player1.classList.toggle("activePlayer");
@@ -50,6 +57,7 @@ function changeColorPlayer() {
     player1.classList.toggle("activePlayer");
   }
 }
+//Get the current score and add it to the total Score of the active player.
 function holding() {
   if (player1.classList.contains("activePlayer")) {
     totalScore1 = totalScore1 + current;
@@ -67,7 +75,7 @@ function holding() {
     win()
   }
 }
-
+// Roll the dice function and put the score in the current Score of the active player.
 function rollDice() {
   let rolled = randomDiceNumber();
   if (rolled == lose) {
@@ -87,5 +95,6 @@ function rollDice() {
     }
   }
 }
+
 rollButton.addEventListener("click", rollDice);
 holdButton.addEventListener("click", holding);
